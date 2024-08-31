02f5a7274de4ccb8e9a22f96f7fb773afee35d4b
Dinesh Nimmagadda <dnimmagadda@detectronic.org>
2024-08-31 02:10:09 +0100

    added js files

---
+++ src/static/js/components/app.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { Login } from './login';
+import { MainPage } from './main_page';
+
+const App: React.FC = () => {
+    return (
+        <Router>
+            <div className="app">
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/login">Login</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+                <Routes>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/" element={<MainPage />} />
+                </Routes>
+            </div>
+        </Router>
+    );
+};
+
+export default App;
