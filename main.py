0ef2f0c203c6db6671d7a08c8ee598e1a132534f
Dinesh Nimmagadda <dnimmagadda@detectronic.org>
2024-09-02 06:58:39 +0100

    updated file structure

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+import uvicorn
 
 app = FastAPI()
 
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+app.mount("/static", StaticFiles(directory="src/static"), name="static")
 
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="src/templates")
 
 
 @app.get("/")
 async def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/login")
+async def login(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8250, reload=True)
 
