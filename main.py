c0f41518a91d1e5b1c16cbf37eea21b159fbc759
Dinesh Nimmagadda <dnimmagadda@detectronic.org>
2024-09-05 03:41:20 +0100

    readded env

--- main.py
+++ main.py
@@ -49,3 +49,6 @@ async def read_index():
 async def health_check():
     return {"status": "healthy"}
 
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8230, workers=1)
